9ccf42c42ce192035d59e541b53a109887ed04ec
Andy Brett <andybrett@gmail.com>
2010-09-18 13:16:31 -0700

    initial commit with better naming

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Andy30::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Andy30::Application.config.secret_token = '72237b3f627a58108e9e553e1fd11d3ee6b7a07f659db49fa547e87a686549dc27eeeed3cc7840df6f5ed24da83762de0df533aa670e844b42600774628aadaa'
