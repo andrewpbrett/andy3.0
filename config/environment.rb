9ec5faabb39b93d5415abc2687e1dd06808be75b
Andy Brett <andy@andybrett.com>
2010-12-08 09:14:10 -0800

    more debugging

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+ENV['PATH'] = "#{ENV['PATH']}:/usr/local/bin"
+
 # Initialize the rails application
 Andy30::Application.initialize!
