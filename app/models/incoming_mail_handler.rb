94e068aca1cedacfbd8989701b427a8f01998a3b
Andy Brett <andy@andybrett.com>
2011-01-04 10:30:08 -0800

    parse email body

--- app/models/incoming_mail_handler.rb
+++ app/models/incoming_mail_handler.rb
@@ -1,9 +1,12 @@
 class IncomingMailHandler < ActionMailer::Base 
   def receive(email) 
     date = email.subject[email.subject.index(',') + 1..email.subject.index('.') - 1]
-    permadate = date.downcase.gsub!("\s", "")
-    # TODO parse email.body
-    Post.create(:permalink => "my-life-#{permadate}", :body => email.body,
+    permadate = date.downcase.gsub!("\s", "") if date
+
+    full_body = email.parts[1].body.decoded
+    body = full_body[0, full_body.index("<div class=\"gmail_quote\"")]
+    
+    Post.create(:permalink => "my-life-#{permadate}", :body => body,
                 :title => "Daily note for #{date}", :published_at => Time.now)
   end 
 end
