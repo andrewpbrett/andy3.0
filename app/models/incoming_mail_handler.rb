3271cbec24086b0e7df8b52df4ed2c599c7669e1
Andy Brett <andy@andybrett.com>
2011-01-05 11:54:21 -0800

    better date parsing/output

--- app/models/incoming_mail_handler.rb
+++ app/models/incoming_mail_handler.rb
@@ -1,12 +1,11 @@
 class IncomingMailHandler < ActionMailer::Base 
   def receive(email) 
-    date = email.subject[email.subject.index(',') + 1..email.subject.index('.') - 1]
-    permadate = date.downcase.gsub!("\s", "") if date
+    date = Date.parse(email.subject[email.subject.index(',') + 1..email.subject.index('.') - 1])
 
     full_body = email.parts[1].body.decoded
     body = full_body[0, full_body.index("<div class=\"gmail_quote\"")]
     
-    Post.create(:permalink => "my-life-#{permadate}", :body => body,
-                :title => "Daily note for #{date}", :published_at => Time.now)
+    Post.create(:permalink => "my-life-#{date.to_s}", :body => body,
+                :title => "Daily note for #{date.to_s}", :published_at => Time.now)
   end 
 end
