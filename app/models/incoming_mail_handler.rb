7cd8312e78bf7fb35f419e4fe68deb14fafc4a38
Andy Brett <andy@andybrett.com>
2011-01-13 10:22:35 -0800

    small tweak to incoming mail handler

--- app/models/incoming_mail_handler.rb
+++ app/models/incoming_mail_handler.rb
@@ -3,7 +3,7 @@ class IncomingMailHandler < ActionMailer::Base
     date = Date.parse(email.subject[email.subject.index(',') + 1..email.subject.index('.') - 1])
 
     full_body = email.parts[1].body.decoded
-    body = full_body[0, full_body.index("<div class=\"gmail_quote\"")]
+    body = full_body.index("EOM") ? full_body[0, full_body.index("EOM")] : full_body
     
     p = Post.create(:permalink => "my-life-#{date.to_s}", :body => body,
                 :title => "Daily note for #{date.to_s}", :published_at => Time.now)
