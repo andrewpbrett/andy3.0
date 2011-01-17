0f48182095936238d5b38bd0170779b9913b7dae
Andy Brett <andy@andybrett.com>
2011-01-16 22:33:19 -0800

    forgot a page title

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,6 @@ class WelcomeController < ApplicationController
     @all_items = Post.recent_public_timeline | Update.recent_public_timeline |
       Image.photostream | Bookmark.recent_public_timeline
     @all_items = @all_items.sort_by { |a| a.published_at }.reverse
+    @page_title = "Andy Brett"
   end
 end
