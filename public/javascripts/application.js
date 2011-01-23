16d728822345a3a016f3fb1ada6f7fd295a11cba
Andy Brett <andy@andybrett.com>
2011-01-22 23:48:59 -0800

    minor tweaks to js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,18 +1,4 @@
 $(document).ready(function(){
-	var path = window.location.pathname.substring(1);
-	if(path.indexOf('/') != -1){
-		path = path.substring(0, path.indexOf('/'));
-	}
-	if($("#" + path).html() == null){
-		$("#writing").addClass('selected');
-	}
-	else{
-		$("#" + path).addClass('selected');
-		document.title = "Andy Brett | " + path.charAt(0).toUpperCase() + path.slice(1)
-	}
-});
-
-$(document).ready(function(){
 	var e = document.getElementById('update_body'),
 	    f = document.getElementById('counter');
 
