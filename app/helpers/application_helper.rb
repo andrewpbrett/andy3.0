72c3e898356d212c0731e3de9086b8cce8a1035c
Andy Brett <andy@andybrett.com>
2011-03-01 20:56:07 -0800

    no more colon on notes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,13 +5,6 @@ module ApplicationHelper
     else
       string.gsub(/(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/([^\s])*)?/ix, '<a href="\0">\0</a>')
     end
-  end  
-  
-  def filter_bookmark_note(string)
-    last = string[string.length - 1..string.length]
-    string = string[0..string.length - 2] if last  == "."
-    string += ":" unless last == ">"
-    string
   end
   
   def display_in_timeline(object)
