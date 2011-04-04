857c5bfcf37c2d8073071546cae825329306a33e
Andy Brett <andy@andybrett.com>
2011-04-04 09:35:20 -0700

    actually logging things, hackedly

--- app/models/incoming_mail_handler.rb
+++ app/models/incoming_mail_handler.rb
@@ -3,8 +3,11 @@ class IncomingMailHandler < ActionMailer::Base
     date = Date.parse(email.subject[email.subject.index(',') + 1..email.subject.index('.') - 1])
 
     full_body = email.parts[1].body.decoded
+    logger.error "parts[0] was #{email.parts[0]}"
+    logger.error "parts[1] was #{email.parts[1]}"    
+    logger.error "full body was #{full_body}"    
     body = full_body.index("EOM") ? full_body[0, full_body.index("EOM")] : full_body
-    logger.debug "body was #{body}"
+    logger.error "body was #{body}"
     p = Post.create(:permalink => "my-life-#{date.to_s}", :body => body,
                 :title => "Daily note for #{date.to_s}", :published_at => Time.now)
     p.tag_list << "daily-email"
