16c932a6e4ddd0afee7ec05f832e2d972433589e
Andy Brett <andy@andybrett.com>
2011-11-27 21:05:25 -0800

    actual deletion

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Andy30::Application.config.secret_token = '508a1cd0d84ba8193c15da7311e237eae3281887b69cdfb9e2515104618577920882af4aefb0b5d1231149ca3d204c9f40305c42c5ae2cf9f68aabf8777f918c'
