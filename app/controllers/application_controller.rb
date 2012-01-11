5c2abae56df5c4ada732255d112a328cfaf2adf4
Andy Brett <andy@andybrett.com>
2012-01-11 15:21:27 -0800

    ugh, not happy about that. will fix it better later

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -53,6 +53,7 @@ class ApplicationController < ActionController::Base
   def find_or_initialize
     i = model_class.find_by_id(params[:id]) if params[:id].present?
     i = model_class.find_by_permalink(params[:id]) if i.blank?
+    i = model_class.find_by_permalink(params[:permalink]) if i.blank?    
     instance_variable_set "@#{model_name}", params[:id].nil? ? model_class.new : i
     render :template => "application/notfound" and return if 
       (params[:permalink].present? or params[:id].present?) and i.blank?
